e992d64fe2f23f00df3edfcf49818b9cdff551fd
anami823 <anami823@gmail.com>
2019-07-24 13:54:57 -0700

    Generate random number

--- objects_lesson.js
+++ objects_lesson.js
@@ -4,15 +4,27 @@ const game = {
 }
 
 game.sallestNum = 0
-game.numGresses = 0
+game.numGuesses = 0
 game.smallestNum = 1
-
-
-//console.log(game)
-
-const catalog = {
-    scooter: 80,
-    headphones: 250,
-    macBook: 3000
+game.play = function () {
+    this.secretNum = Math.floor((Math.random() * this.biggestNum - this.smallestNum + 1)) //return number between 0 - .99999
 }
 
+game.play()
+console.log(game.secretNum)
+
+/*
+const catalog = {
+    'ABC-123': 79.99,
+    'CAT-777': 249.00,
+    'FUN-789': 3000
+}
+
+let sku = ''
+
+while (sku !== 'quit') {
+    sku = prompt('Ener SKU or "quit" to exit: ')
+    let price = catalog[sku]
+    alert(`The price of ${sku} is ${price}`)
+}
+ */
